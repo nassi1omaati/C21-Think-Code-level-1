ce5ea952d047a1f29833570057cb431ac0334158
Nassim1Omaati <2151890@talnet.nl>
2022-01-26 14:05:42 +0100

    eindopdracht opnieuw afgemaakt.

--- eindopdracht/eindopdracht.py
+++ eindopdracht/eindopdracht.py
@@ -1,5 +1,26 @@
-num1 = input("enter a number: ")
-num2 = input("enter another number: ")
-result = float(num1) + float(num2)
+print("selecteer een print:")
+print("1. PLUS")
+print("2. AFTREKKEN")
+print("3. VERMENIGVULDIGEN")
+print("4. VERDELING")
 
-print(result)
+operation = input()
+
+if operation == "1":
+    num1 = input("Kies een nummer: ")
+    num2 =input("Kies een tweede nummer: ")
+    print("De som is " + str(int(num1) + int(num2)))
+elif operation == "2":
+    num1 = input("Kies een nummer: ")
+    num2 =input("Kies een tweede nummer: ")
+    print("Het verschil is " + str(int(num1) - int(num2)))
+elif operation == "3":
+    num1 = input("Kies een nummer ")
+    num2 =input("Kies een tweede nummer: ")
+    print("Het antwoord is " + str(int(num1) * int(num2)))
+elif operation == "4":
+    num1 = input("Kies een nummer: ")
+    num2 =input("Kies een tweede nummer: ")
+    print("Het resultaat is " + str(int(num1) / int(num2)))
+else:
+    print("syntax error")
